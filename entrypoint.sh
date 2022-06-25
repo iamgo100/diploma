c1d7da11372eb9704f497817565f28c88e65e4b4
iamgo100 <gufkz.tkrbyf@yandex.ru>
2022-06-25 19:18:00 +0300

    fixed wrapper of server ()time 2

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 
-gunicorn BeautyManageService.wsgi:application --bind 5.53.125.221:8000
+gunicorn BeautyManageService.wsgi:application --bind 0.0.0.0:8000
