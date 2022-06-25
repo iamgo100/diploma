0a9c74451f77edaf117fd240ba4e2b5cb21ceb34
iamgo100 <gufkz.tkrbyf@yandex.ru>
2022-06-25 18:56:42 +0300

    fixed gunicorn start

--- DiplomaProject/gunicorn_config.py
+++
@@ -1,8 +0,0 @@
-command = 'gunicorn'
-pythonpath = '/home/www/code/project/project'
-bind = '0.0.0.0:8000'
-workers = 3
-user = 'root'
-limit_request_fields = 32000
-limit_request_field_size = 0
-raw_env = 'DJANGO_SETTINGS_MODULE=BeautyManageService.settings'
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,4 @@
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 
-# gunicorn BeautyManageService.wsgi:application --bind 0.0.0.0:8000
-exec gunicorn -c "/DiplomaProject/gunicorn_config.py" BeautyManageService.wsgi
+gunicorn BeautyManageService.wsgi:application --bind beautymanage.ru:8000
