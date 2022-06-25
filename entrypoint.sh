153aa1ee0e391536e64d26e7234146002c41bac9
iamgo100 <gufkz.tkrbyf@yandex.ru>
2022-06-25 15:18:04 +0300

    fixed default.conf

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 
-gunicorn BeautyManageService.wsgi:application --bind beautymanage.ru:8000
+gunicorn BeautyManageService.wsgi:application --bind 0.0.0.0:8000
